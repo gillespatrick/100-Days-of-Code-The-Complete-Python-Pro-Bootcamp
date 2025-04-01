62027e88583f443d0cbfcd25bc377c5de53149bd
Gilles Patrick <ngondogillespatrick@gmail.com>
2025-03-31 22:20:49 -0400

    Learn about Input function, Variable,Concatenation and more

---
+++ Day 1 : Working with variables in Python to manage Data/project.py
@@ -0,0 +1,12 @@
+#Band Name Generator Project
+# Description
+# 1- Create a greeting for your program
+# 2- Ask the user for the city that they grew up in and store it in a variable
+# 3- Ask the user for the name of pet and store it a variable
+# 4- Combine the name of their city and pet and show them their band name
+
+
+print("WELCOME TO MY BAND NAME GENERATOR PROJECT\n")
+cityName = (input("Which city your grew up in?\n"))
+petName = (input("What is your name pet?\n"))
+print("Your band name is "+ petName +" "+ cityName)
--- Day 1 : Working with variables in Python to manage Data/task.py
+++ Day 1 : Working with variables in Python to manage Data/task.py
@@ -1,1 +1,6 @@
-print("Hello world")
+# This line of code will take an using input() function
+
+
+name = "Gilles Patrick"
+print(name)
+print(len(name))
